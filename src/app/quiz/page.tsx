83887ef3406735bd8b48d0711318d946bdefb1f5
dnadzialek <d.nadzialek@gmail.com>
2026-09-05 15:33:20 +0200

    Gotowa wersja strony ze zmienionym motywem

---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,9 @@
+export default function QuizPage() {
+  return (
+    <iframe 
+      src="/gra_pozycje.html" 
+      className="w-full h-full border-none"
+      title="U8 Haasdonk Quiz"
+    />
+  );
+}
---
+++ src/app/training/page.tsx
@@ -0,0 +1,9 @@
+export default function TrainingPage() {
+  return (
+    <iframe 
+      src="/gra_3d.html" 
+      className="w-full h-full border-none"
+      title="U8 Haasdonk 3D Training"
+    />
+  );
+}
